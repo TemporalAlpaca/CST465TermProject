9f427cc084fef925ab4a1991fab7ea14fc7b27c3
Caleb Kauffman <ckauffman16@gmail.com>
2018-10-17 20:33:30 -0700

    lab 2 update

---
+++ Assignment1/Models/ContactModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment1.Models
+{
+    public class ContactModel
+    {
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public string phoneNumber { get; set; }
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public string message { get; set; }
+
+    }
+}
